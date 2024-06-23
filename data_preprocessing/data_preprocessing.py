d261b9d68842ba44ec84e356e2ad593d0e1b31f0
kotatkow <ghkjs0810@gmail.com>
2024-06-23 20:16:17 +0900

    write data preprocessing code

--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -2,9 +2,73 @@ import pandas as pd
 from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.preprocessing import MinMaxScaler
 
-#Load datasets
-def load_data(C:/Users/ghkjs/Inflation-Prediction-Model/economic_data):
-              data = pd.read_excel(C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/ppi_data.csv)
-              data['date'] = pd.to_datetime(data['date'])
-              data.set_index('date', inplace=True)
-              return data
+# Load datasets
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    data['date'] = pd.to_datetime(data['date'])
+    data.set_index('date', inplace=True)
+    return data
+
+cpi_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/cpi_data.csv')
+agriculture_price_index_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/agriculture_price_index_data.csv')
+core_cpi_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/core_cpi_data.csv')
+core_pce_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/core_pce_data.csv')
+crude_oil_price_index_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/crude_oil_price_index_data.csv')
+gdp_deflator_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/gdp_deflator_data.csv')
+hpi_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/hpi_data.csv')
+industrial_price_index  = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/industrial_price_index_data.csv')
+interest_rates_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/interest_rates_data.csv')
+m2_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/m2_data.csv')
+nominal_gdp_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/nominal_gdp_data.csv')
+pce_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/pce_data.csv')
+pce_percentage_change = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/pce_percentage_change.csv')
+ppi_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/ppi_data.csv')
+real_gdp_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/real_gdp_data.csv')
+seasonally_adjusted_ppi_data = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/seasonally_adjusted_ppi_data.csv')
+velocity_of_money = load_data('C:/Users/ghkjs/Inflation-Prediction-Model/economic_data/velocity_of_money.csv')
+
+economic_data=[cpi_data, agriculture_price_index_data, core_cpi_data, core_pce_data, crude_oil_price_index_data,
+                 gdp_deflator_data, hpi_data, industrial_price_index, interest_rates_data, 
+                 m2_data, nominal_gdp_data, pce_data, pce_percentage_change, ppi_data, real_gdp_data, 
+                 seasonally_adjusted_ppi_data, velocity_of_money]
+
+# Handle missing values
+def handle_missing_values(data):
+    return data.ffill().bfill()
+
+cpi_data = handle_missing_values(cpi_data)
+agriculture_price_index_data = handle_missing_values(agriculture_price_index_data)
+core_cpi_data = handle_missing_values(core_cpi_data)
+crude_oil_price_index_data = handle_missing_values(crude_oil_price_index_data)
+gdp_deflator_data = handle_missing_values(gdp_deflator_data)
+hpi_data = handle_missing_values(hpi_data)
+industrial_price_index = handle_missing_values(industrial_price_index)
+interest_rates_data = handle_missing_values(interest_rates_data)
+m2_data = handle_missing_values(m2_data)
+nominal_gdp_data = handle_missing_values(nominal_gdp_data)
+pce_data = handle_missing_values(pce_data)
+pce_percentage_change = handle_missing_values(pce_percentage_change)
+ppi_data = handle_missing_values(ppi_data)
+real_gdp_data = handle_missing_values(real_gdp_data)
+seasonally_adjusted_ppi_data = handle_missing_values(seasonally_adjusted_ppi_data)
+velocity_of_money = handle_missing_values(velocity_of_money)
+
+# Seasonal adjustment
+def seasonal_adjustment(data, column, period=12):
+    decomposition = seasonal_decompose(data[column], model='additive', period=period)
+    return data[column] - decomposition.seasonal
+
+cpi_data['seasonally_adjusted'] = seasonal_adjustment(cpi_data, 'value')
+agriculture_price_index_data['seasonally_adjusted'] = seasonal_adjustment(agriculture_price_index_data,
+                                                                          'value')
+core_cpi_data['seasonally_adjusted'] = seasonal_adjustment(core_cpi_data, 'value')
+core_pce_data['seasonally_adjusted'] = seasonal_adjustment(core_pce_data, 'value')
+crude_oil_price_index_data['seasonally_adjusted'] = seasonal_adjustment(crude_oil_price_index_data,
+                                                                        'value')
+gdp_deflator_data['seasonally_adjusted'] = seasonal_adjustment(gdp_deflator_data,'value')
+hpi_data['seasonally_adjusted'] = seasonal_adjustment(hpi_data, 'value')
+industrial_price_index['seasonally_adjusted'] = seasonal_adjustment(industrial_price_index, 'value')
+m2_data['seasonally_adjusted'] = seasonal_adjustment(m2_data, 'value')
+pce_data['seasonally_adjusted'] = seasonal_adjustment(pce_data, 'value')
+ppi_data['seasonally_adjusted'] = seasonal_adjustment(ppi_data, 'value')
+real_gdp_data['seasonaly_adjusted'] = seasonal_adjustment(real_gdp_data, 'value')
